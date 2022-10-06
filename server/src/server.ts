a8f16c3b17f72ba698ec103218fcef5f2f0afc6c
IT-ess <me@aloiseau.com>
2022-10-06 16:56:36 +0200

    rearranged files for mono-repo

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 
 async function startServer() {
   server.listen(process.env.PORT, () => {
-    console.log(`Listening on port 3000...`)
+    console.log(`Listening on port ${process.env.PORT}...`)
   })
 }
 
