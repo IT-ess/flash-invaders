210ea7c9569c181d64012a89b84b6847a6115e9f
IT-ess <me@aloiseau.com>
2022-10-10 14:34:22 +0200

    added docker-compose to get a sql image + sequelize package

---
+++ server/src/utils/SequelizeInstance.ts
@@ -0,0 +1,20 @@
+import { Config } from "../config/Config"
+import { Sequelize } from "sequelize"
+
+export async function createSequelizeInstance(config: Config): Promise<Sequelize> {
+  const sequelize = new Sequelize(config.mysql.database, config.mysql.user, config.mysql.password, {
+    host: config.mysql.host,
+    port: config.mysql.port,
+    dialect: "mysql",
+  })
+
+  try {
+    await sequelize.authenticate()
+    console.log("Connection has been established successfully.")
+    await sequelize.sync({ force: true })
+    console.log("All models were synchronized")
+  } catch (error) {
+    console.error("Unable to connect to the database:", error)
+  }
+  return sequelize
+}
