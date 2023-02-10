d729065dae400a8fe933822a8e566b481d69039e
Itess <me@aloiseau.com>
2023-02-10 19:05:15 +0100

    Added packages and modified config

--- invaders-front/src/routes/+layout.svelte
+++ invaders-front/src/routes/+layout.svelte
@@ -1,6 +1,14 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/gallery">galerie</a>
-</nav>
+<script>
+	import '../app.css';
+	
+</script>
+
+<div class="bg-slate-900">
+	<nav>
+		<a href="/">home</a>
+		<a href="/gallery">galerie</a>
+	</nav>
+</div>
+
 
 <slot />
