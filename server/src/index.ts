65e5e927b4130726d569f22af16f80fc6d91050d
Itess <me@aloiseau.com>
2023-03-10 15:07:11 +0100

    Cleaned up old project

--- server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { start } from "./builder"
-
-start().catch((error) => {
-  console.error("Fatal error in express server, Stopping the server.")
-  console.error(error)
-  process.exit(1)
-})
--- server/src/utils/RedisLoader.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "redis-om"
-import { Config } from "../config/Config"
-
-export async function getRedisOMClient(config: Config): Promise<Client> {
-  const url = config.redis.url
-  /* create and open the Redis OM Client */
-  return new Client().open(url)
-}
