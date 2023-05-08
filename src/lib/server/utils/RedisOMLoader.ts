8a8e487eea9d307b617f88fc25a0ed201545bab5
Itess <me@aloiseau.com>
2023-05-08 12:28:38 +0200

    redis url fix

--- src/lib/server/utils/RedisOMLoader.ts
+++ src/lib/server/utils/RedisOMLoader.ts
@@ -1,4 +1,6 @@
 import { Client } from 'redis-om';
 import { REDIS_URL } from '$env/static/private';
 
-export const redisOMClient = await new Client().open(REDIS_URL);
+const urlOrDefault = REDIS_URL === '' ? undefined : REDIS_URL;
+
+export const redisOMClient = await new Client().open(urlOrDefault);
