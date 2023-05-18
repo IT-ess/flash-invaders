907157a5f33dcaa50098ce959e002e20ebf564fa
Itess <me@aloiseau.com>
2023-05-18 12:42:11 +0200

    test

--- src/routes/[lang]/context/[id=integer]/quiz/+page.svelte
+++ src/routes/[lang]/context/[id=integer]/quiz/+page.svelte
@@ -18,10 +18,10 @@
 
 	let answers = new Array(questions.length).fill(null);
 	let questionPointer = 0;
-	let score = 0
+	let score = 0;
 </script>
 
-<div class="fixed top-0 left-0 w-screen h-screen z-0">
+<div class="relative w-screen h-screen flex flex-col">
 	{#if !(questionPointer > answers.length - 1)}
 		<div class="w-full h-full flex flex-col">
 			<div class="my-0">
@@ -51,29 +51,31 @@
 					</Button>
 				{/each}
 			</div>
-			<div class="w-full h-20 flex justify-center items-center bg-gray-200">
-				<Button
-					on:click={() => {
-						if (questions[questionPointer].correctIndex == answers[questionPointer]) {
-							score += (questionPointer + 1) * 10; // the harder are the questions, the more points you get
-						}
-						questionPointer++;
-					}}
-				>
-					{$t('quiz.next')}
-					<svg
-						aria-hidden="true"
-						class="ml-2 -mr-1 w-5 h-5"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-							clip-rule="evenodd"
-						/></svg
+			<div class="w-full flex-shrink-0 basis-20 justify-center items-center bg-gray-200">
+				<div class="w-full text-center">
+					<Button
+						on:click={() => {
+							if (questions[questionPointer].correctIndex == answers[questionPointer]) {
+								score += (questionPointer + 1) * 10; // the harder are the questions, the more points you get
+							}
+							questionPointer++;
+						}}
 					>
-				</Button>
+						{$t('quiz.next')}
+						<svg
+							aria-hidden="true"
+							class="ml-2 -mr-1 w-5 h-5"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								fill-rule="evenodd"
+								d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+								clip-rule="evenodd"
+							/></svg
+						>
+					</Button>
+				</div>
 			</div>
 		</div>
 	{:else}
